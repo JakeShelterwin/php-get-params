eff765ce34a7deac6a8e11780491b5fe7b2334a8
Giacomo <kirtash.k@live.it>
2020-05-05 16:12:04 +0200

    Esercizio Saluta Interlocutore

---
+++ es 1/index.html.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Esercizio PHP 1</title>
+  </head>
+  <body>
+    <!-- - passare come argomenti in GET nome e cognome; e salutare l'interlocutore -->
+    <?php
+      $nome=$_GET["nome"];
+      $cognome=$_GET["cognome"];
+     ?>
+
+    <h1>Ciao
+      <?php
+      echo $nome;
+      echo ' '.$cognome;
+      ?>
+    </h1>
+  </body>
+</html>
