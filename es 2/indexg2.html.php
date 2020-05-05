b40a9ff9b1ad6c7eb02a5b5f3fbdd2e5c56472be
Giacomo <kirtash.k@live.it>
2020-05-05 16:21:35 +0200

    Esercizio Controlla validità Mail

--- es 2/indexg2.html.php
+++ es 2/indexg2.html.php
@@ -9,39 +9,40 @@
     <?php
       $mail=$_GET["mail"];
      ?>
+     <div class="check">
+       <h1>
+         <?php
+         //controllo in che posizione si trova @ e . nella mail
+         $haChioccola = strpos($mail, "@");
+         $haPunto = strpos($mail, ".");
 
-    <h1>
-      <?php
-      //controllo in che posizione si trova @ e . nella mail
-      $haChioccola = strpos($mail, "@");
-      $haPunto = strpos($mail, ".");
+         //strpos restituisce la posizione (un int che parte da 0 ) se il carattere esiste, altrimenti è false
+         if($haChioccola === false || $haPunto === false)  {
+           echo "la mail non è valida: KO";
+         } else {
+           echo "la mail è valida: OK";
+         }
 
-      //strpos restituisce la posizione (un int che parte da 0 ) se il carattere esiste, altrimenti è false
-      if($haChioccola === false || $haPunto === false)  {
-        echo "la mail non è valida: KO";
-      } else {
-        echo "la mail è valida: OK";
-      }
+         /*
+         //altro metodo:
+         //siccome strpos restituisce un valore che parte da 0, se uno dei due caratteri "trovati" ha indice 0
+         //li aumento di 1, così che l'if successivo non abbia problemi se @ o . sono i primi caratteri della mail
+         if ($haChioccola===0 || $haPunto===0) {
+           $haChioccola++;
+           $haPunto++;
+         }
+         // echo $haChioccola." ";
+         // echo $haPunto." ";
 
-      /*
-      //altro metodo:
-      //siccome strpos restituisce un valore che parte da 0, se uno dei due caratteri "trovati" ha indice 0
-      //li aumento di 1, così che l'if successivo non abbia problemi se @ o . sono i primi caratteri della mail
-      if ($haChioccola===0 || $haPunto===0) {
-        $haChioccola++;
-        $haPunto++;
-      }
-      // echo $haChioccola." ";
-      // echo $haPunto." ";
+         if ($haChioccola && $haPunto){
+           echo "la mail è valida: OK";
+         } else {
+           echo "la mail non è valida: KO";
+         }
+         */
 
-      if ($haChioccola && $haPunto){
-        echo "la mail è valida: OK";
-      } else {
-        echo "la mail non è valida: KO";
-      }
-      */
-
-      ?>
-    </h1>
+         ?>
+       </h1>
+     </div>
   </body>
 </html>
